fca8d9ecc0491375c1dd003b9a0c68ef26af3fe7
gregwhitaker <gwhitake@gmail.com>
2019-02-10 15:36:39 -0800

    Added buildSrc to build classpath

--- buildSrc/src/main/java/example/buildsrc/HelloWorldTask.java
+++ buildSrc/src/main/java/example/buildsrc/HelloWorldTask.java
@@ -1,8 +1,12 @@
 package example.buildsrc;
 
 import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
 
 public class HelloWorldTask extends DefaultTask {
 
-
+    @TaskAction
+    public void run() {
+        System.out.println("Hello from task " + getPath() + "!");
+    }
 }
